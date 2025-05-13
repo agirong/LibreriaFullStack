f878df39b9b801d03df8d5a959389cd3196b69cb
agirong <aogarrido.g@gmail.com>
2025-05-13 15:11:31 -0600

    Crear la base de datos con codefirst creando los Models y Data que contiene el Contexto. Tambien se ageregaron los paquetes necesarios.

---
+++ LibreriaFullStack/Data/LibreriaContext.cs
@@ -0,0 +1,16 @@
+﻿using Libreria.Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Libreria.Backend.Data
+{
+    public class LibreriaContext : DbContext
+    {
+        public LibreriaContext(DbContextOptions<LibreriaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Libro> Libros { get; set; }
+        public DbSet<Autor> Autors { get; set; }
+    }
+}
---
+++ LibreriaFullStack/Models/Libro.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Libreria.Backend.Models
+{
+    public class Libro
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int LibroID { get; set; }
+        [Required]
+        public string? Titulo { get; set; }
+        public int Anio { get; set; }
+        public string? Genero { get; set; }
+        public string? Editorial { get; set; }
+        public int Paginas { get; set; }
+        public int AutorID { get; set; }
+
+        [ForeignKey("AutorID")]
+        public virtual Autor Autor { get; set; }
+    }
+}
