fb4d00263d65013b998dc48d9086ee9a968cec0c
agirong <aogarrido.g@gmail.com>
2025-05-30 00:23:05 -0600

    Service Autor

---
+++ LibreriaFullStack/Service/IServiceAutor.cs
@@ -0,0 +1,11 @@
+﻿using Libreria.Backend.DTOs.Autor;
+using Libreria.Backend.Utils;
+
+namespace Libreria.Backend.Service
+{
+    public interface IServiceAutor
+    {
+        public GeneralResponse ListarAutores();
+        public GeneralResponse RegistrarAutor(AutorDTO autor);
+    }
+}
