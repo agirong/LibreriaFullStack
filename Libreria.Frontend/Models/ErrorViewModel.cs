54ff26625f6df438b9606877087a00c08ec1eb75
agirong <aogarrido.g@gmail.com>
2025-05-30 23:22:49 -0600

    Borrar  mvc frontend

--- Libreria.Frontend/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Libreria.Frontend.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
