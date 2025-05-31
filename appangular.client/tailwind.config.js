9c26ce87ccb2e95ccd80755c2ede54650bd6baea
agirong <aogarrido.g@gmail.com>
2025-05-30 23:29:22 -0600

    Configurar tailwinds.css

--- appangular.client/tailwind.config.js
+++ appangular.client/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,ts}", // archivos HTML y TypeScript en la carpeta src
+  ],
   theme: {
     extend: {},
   },
