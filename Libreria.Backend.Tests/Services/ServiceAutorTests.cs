adc8913f87c25aa45290ac68e2f144e2ac2693f6
agirong <aogarrido.g@gmail.com>
2025-06-08 13:10:51 -0600

    Añadi pruebas unitarias para el Services AutorService y crear la clase de pruebas.

---
+++ Libreria.Backend.Tests/Services/ServiceAutorTests.cs
@@ -0,0 +1,31 @@
+﻿using Libreria.Backend.DTOs.Autor;
+using FluentAssertions;
+using Libreria.Backend.Models;
+using Libreria.Backend.ServiceImpl;
+using Libreria.Backend.Service;
+using Libreria.Backend.RepositoryImpl;
+using Libreria.Backend.Repository;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Libreria.Backend.Utils;
+
+namespace Libreria.Backend.Tests.Services
+{
+    public class ServiceAutorTests
+    {
+        //Mock para simular el repositotry
+        private readonly Mock<IRepositoryAutor> _mockAutorRepository;
+        private readonly ServiceAutorImpl _sut;
+
+        public ServiceAutorTests()
+        {
+            _mockAutorRepository = new Mock<IRepositoryAutor>();
+            _sut = new ServiceAutorImpl(_mockAutorRepository.Object); 
+        }
+  
+    }
+}
